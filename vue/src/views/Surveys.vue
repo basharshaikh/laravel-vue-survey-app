b7b129b6280dfb6963f643e49d45c087cf097767
BasharShaikh <basharshaikh444@gmail.com>
2023-02-27 13:23:57 +0600

    condition

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -50,7 +50,7 @@
           </nav>
         </div>
       </div>
-      <div class="min-h-screen">
+      <div v-else class="min-h-screen">
         <p class="text-2xl text-center">You not added a survey yet...</p>
       </div>
     </div>
